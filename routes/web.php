c7b08c0613e9b0327d19f0303824966019dc7baf
Marlon F. Minotti <marlon.minotti@gmail.com>
2018-09-23 16:08:05 -0400

    Alteração Rotas e Controller

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+/*Site*/
+Route::group(['namespace' => 'Site'], function(){
+    Route::get('/', 'SiteController@index')->name('site.home');
 });
 
 Auth::routes();
 
-Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function(){
+/*Admin*/
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => 'auth'], function(){
     Route::get('/', 'HomeController@index')->name('home');
     Route::get('/sensores', 'HomeController@sensores')->name('sensores');
 });
