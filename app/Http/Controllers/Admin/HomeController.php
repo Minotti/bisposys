d2f742ca608690ea59c906c3f8a088c80ccc5b15
Marlon F. Minotti <marlon.minotti@gmail.com>
2018-09-23 16:13:24 -0400

    Alteração pastas

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -24,11 +24,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('admin.home');
     }
 
     public function sensores()
     {
-        return view('home');
+        return view('admin.home');
     }
 }
